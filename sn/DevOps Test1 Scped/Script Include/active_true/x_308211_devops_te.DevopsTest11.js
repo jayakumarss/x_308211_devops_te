c198c81843bd71306f4f49a666c7296369ad6478
System Administrator <admin@example.com>
2019-11-21 08:48:47 -0500

    Changes by 'System Administrator' on 'DevOps Test1 Scped - 1.0.0' -- Build #1

---
+++ sn/DevOps Test1 Scped/Script Include/active_true/x_308211_devops_te.DevopsTest11.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : DevOps Test1 Scped
+ * ClassName   : sys_script_include
+ * Created On  : 2019-11-21 13:43:08
+ * Created By  : admin
+ * Updated On  : 2019-11-21 13:43:08
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=3ef49bf4dbdd0010343f58b3ca9619e3
+ */
+var DevopsTest11 = Class.create();
+DevopsTest11.prototype = {
+    initialize: function() {
+    },
+
+    type: 'DevopsTest11'
+};
